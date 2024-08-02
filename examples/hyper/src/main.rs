db4b72d39860c86833c34fb0da9fc5817b21eb21
Cathal Mullan <contact@cathal.dev>
2024-08-02 18:01:08 +0100

    Clean up example.

--- examples/hyper/src/main.rs
+++ examples/hyper/src/main.rs
@@ -1,135 +1,80 @@
 #![allow(clippy::unused_async)]
 
 use bytes::{Buf, Bytes};
-use http_body_util::{BodyExt, Full};
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{body::Incoming as IncomingBody, header, Method, Request, Response, StatusCode};
-use hyper_util::rt::TokioIo;
-use std::net::SocketAddr;
-use tokio::net::{TcpListener, TcpStream};
+use http_body_util::{combinators::BoxBody, BodyExt, Full};
+use hyper::{body::Incoming as IncomingBody, header, service::service_fn, Method, Request, Response, StatusCode};
+use hyper_util::{
+    rt::{TokioExecutor, TokioIo},
+    server::conn::auto::Builder,
+};
+use std::{
+    convert::Infallible,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+};
+use tokio::{net::TcpListener, task::JoinSet};
 
-type GenericError = Box<dyn std::error::Error + Send + Sync>;
-type Result<T> = std::result::Result<T, GenericError>;
-type BoxBody = http_body_util::combinators::BoxBody<Bytes, hyper::Error>;
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1337);
+    let listener = TcpListener::bind(&socket).await?;
+    println!("Listening on http://{socket}");
 
-static INDEX: &[u8] = b"<a href=\"test.html\">test.html</a>";
-static INTERNAL_SERVER_ERROR: &[u8] = b"Internal Server Error";
-static NOTFOUND: &[u8] = b"Not Found";
-static POST_DATA: &str = r#"{"original": "data"}"#;
-static URL: &str = "http://127.0.0.1:1337/json_api";
+    let mut join_set = JoinSet::new();
+    loop {
+        let Ok((stream, _)) = listener.accept().await else {
+            continue;
+        };
 
-async fn client_request_response() -> Result<Response<BoxBody>> {
-    let req = Request::builder()
-        .method(Method::POST)
-        .uri(URL)
-        .header(header::CONTENT_TYPE, "application/json")
-        .body(Full::new(Bytes::from(POST_DATA)))
-        .unwrap();
-
-    let host = req
-        .uri()
-        .host()
-        .expect("uri has no host");
-    let port = req
-        .uri()
-        .port_u16()
-        .expect("uri has no port");
-    let stream = TcpStream::connect(format!("{host}:{port}")).await?;
-    let io = TokioIo::new(stream);
-
-    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
-
-    tokio::task::spawn(async move {
-        if let Err(err) = conn.await {
-            println!("Connection error: {err:?}");
-        }
-    });
-
-    let web_res = sender.send_request(req).await?;
-
-    let res_body = web_res.into_body().boxed();
-
-    Ok(Response::new(res_body))
+        join_set.spawn(async move {
+            let _ = Builder::new(TokioExecutor::new())
+                .serve_connection(TokioIo::new(stream), service_fn(route_request))
+                .await;
+        });
+    }
 }
 
-async fn api_post_response(req: Request<IncomingBody>) -> Result<Response<BoxBody>> {
-    // Aggregate the body...
-    let whole_body = req.collect().await?.aggregate();
-    // Decode as JSON...
-    let mut data: serde_json::Value = serde_json::from_reader(whole_body.reader())?;
-    // Change the JSON...
-    data["test"] = serde_json::Value::from("test_value");
-    // And respond with the new JSON.
-    let json = serde_json::to_string(&data)?;
+async fn route_request(request: Request<IncomingBody>) -> Result<Response<BoxBody<Bytes, Infallible>>, anyhow::Error> {
+    // TODO: Use router here!
+    match (request.method(), request.uri().path()) {
+        (&Method::GET, "/") => api_get_response().await,
+        (&Method::POST, "/") => api_post_response(request).await,
+        _ => {
+            let json = serde_json::json!({ "route": "not_found" });
+
+            let body = Full::new(Bytes::from(json.to_string()));
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(body.boxed())?;
+
+            Ok(response)
+        }
+    }
+}
+
+async fn api_post_response(
+    request: Request<IncomingBody>,
+) -> Result<Response<BoxBody<Bytes, Infallible>>, anyhow::Error> {
+    let buffer = request.collect().await?.aggregate();
+
+    let mut json: serde_json::Value = serde_json::from_reader(buffer.reader())?;
+    json["test"] = serde_json::Value::from("test_value");
+
+    let body = Full::new(Bytes::from(json.to_string()));
     let response = Response::builder()
         .status(StatusCode::OK)
         .header(header::CONTENT_TYPE, "application/json")
-        .body(full(json))?;
+        .body(body.boxed())?;
+
     Ok(response)
 }
 
-async fn api_get_response() -> Result<Response<BoxBody>> {
-    let data = vec!["foo", "bar"];
-    let res = serde_json::to_string(&data).map_or_else(
-        |_| {
-            Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(full(INTERNAL_SERVER_ERROR))
-                .unwrap()
-        },
-        |json| {
-            Response::builder()
-                .header(header::CONTENT_TYPE, "application/json")
-                .body(full(json))
-                .unwrap()
-        },
-    );
-    Ok(res)
-}
+async fn api_get_response() -> Result<Response<BoxBody<Bytes, Infallible>>, anyhow::Error> {
+    let json = serde_json::json!({ "hello": "world" });
 
-async fn response_examples(req: Request<IncomingBody>) -> Result<Response<BoxBody>> {
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/" | "/index.html") => Ok(Response::new(full(INDEX))),
-        (&Method::GET, "/test.html") => client_request_response().await,
-        (&Method::POST, "/json_api") => api_post_response(req).await,
-        (&Method::GET, "/json_api") => api_get_response().await,
-        _ => {
-            // Return 404 not found response.
-            Ok(Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(full(NOTFOUND))
-                .unwrap())
-        }
-    }
-}
+    let body = Full::new(Bytes::from(json.to_string()));
+    let response = Response::builder()
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(body.boxed())?;
 
-fn full<T: Into<Bytes>>(chunk: T) -> BoxBody {
-    Full::new(chunk.into())
-        .map_err(|never| match never {})
-        .boxed()
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let addr: SocketAddr = "127.0.0.1:1337".parse().unwrap();
-
-    let listener = TcpListener::bind(&addr).await?;
-    println!("Listening on http://{addr}");
-
-    loop {
-        let (stream, _) = listener.accept().await?;
-        let io = TokioIo::new(stream);
-
-        tokio::task::spawn(async move {
-            let service = service_fn(response_examples);
-
-            if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service)
-                .await
-            {
-                println!("Failed to serve connection: {err:?}");
-            }
-        });
-    }
+    Ok(response)
 }
