531efee7bc765a5f5ff184a32ead7b861e58b8aa
Cathal Mullan <contact@cathal.dev>
2024-08-02 23:22:37 +0100

    Update router versions in benchmarks.

--- benches/matchit.rs
+++ benches/matchit.rs
@@ -6,61 +6,66 @@ use matchit_routes::call;
 
 pub mod matchit_routes;
 
-fn compare_routers(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Compare Routers");
+fn benchmark(criterion: &mut Criterion) {
+    let mut group = criterion.benchmark_group("matchit benchmarks");
 
-    let mut wayfind = wayfind::router::Router::new();
-    for route in register!(brackets) {
-        wayfind.insert(route, true);
-    }
-    group.bench_function("wayfind", |b| {
-        b.iter(|| {
+    group.bench_function("wayfind", |bencher| {
+        let mut wayfind = wayfind::router::Router::new();
+        for route in register!(brackets) {
+            wayfind.insert(route, true);
+        }
+
+        bencher.iter(|| {
             for route in black_box(call()) {
                 black_box(wayfind.matches(route).unwrap());
             }
         });
     });
 
-    let mut matchit = matchit::Router::new();
-    for route in register!(brackets) {
-        matchit.insert(route, true).unwrap();
-    }
-    group.bench_function("matchit", |b| {
-        b.iter(|| {
+    group.bench_function("matchit", |bencher| {
+        let mut matchit = matchit::Router::new();
+        for route in register!(brackets) {
+            matchit.insert(route, true).unwrap();
+        }
+
+        bencher.iter(|| {
             for route in black_box(call()) {
                 black_box(matchit.at(route).unwrap());
             }
         });
     });
 
-    let mut path_tree = path_tree::PathTree::new();
-    for route in register!(colon) {
-        path_tree.insert(route, true);
-    }
-    group.bench_function("path-tree", |b| {
-        b.iter(|| {
+    group.bench_function("path-tree", |bencher| {
+        let mut path_tree = path_tree::PathTree::new();
+        for route in register!(colon) {
+            let _ = path_tree.insert(route, true);
+        }
+
+        bencher.iter(|| {
             for route in black_box(call()) {
                 black_box(path_tree.find(route).unwrap());
             }
         });
     });
 
-    let gonzales = gonzales::RouterBuilder::new().build(register!(brackets));
-    group.bench_function("gonzales", |b| {
-        b.iter(|| {
+    group.bench_function("gonzales", |bencher| {
+        let gonzales = gonzales::RouterBuilder::new().build(register!(brackets));
+
+        bencher.iter(|| {
             for route in black_box(call()) {
                 black_box(gonzales.route(route).unwrap());
             }
         });
     });
 
-    let mut actix = actix_router::Router::<bool>::build();
-    for route in register!(brackets) {
-        actix.path(route, true);
-    }
-    let actix = actix.finish();
-    group.bench_function("actix", |b| {
-        b.iter(|| {
+    group.bench_function("actix-router", |bencher| {
+        let mut actix = actix_router::Router::<bool>::build();
+        for route in register!(brackets) {
+            actix.path(route, true);
+        }
+        let actix = actix.finish();
+
+        bencher.iter(|| {
             for route in black_box(call()) {
                 let mut path = actix_router::Path::new(route);
                 black_box(actix.recognize(&mut path).unwrap());
@@ -68,21 +73,23 @@ fn compare_routers(c: &mut Criterion) {
         });
     });
 
-    let regex_set = regex::RegexSet::new(register!(regex)).unwrap();
-    group.bench_function("regex", |b| {
-        b.iter(|| {
+    group.bench_function("regex", |bencher| {
+        let regex_set = regex::RegexSet::new(register!(regex)).unwrap();
+
+        bencher.iter(|| {
             for route in black_box(call()) {
                 black_box(regex_set.matches(route));
             }
         });
     });
 
-    let mut route_recognizer = route_recognizer::Router::new();
-    for route in register!(colon) {
-        route_recognizer.add(route, true);
-    }
-    group.bench_function("route-recognizer", |b| {
-        b.iter(|| {
+    group.bench_function("route-recognizer", |bencher| {
+        let mut route_recognizer = route_recognizer::Router::new();
+        for route in register!(colon) {
+            route_recognizer.add(route, true);
+        }
+
+        bencher.iter(|| {
             for route in black_box(call()) {
                 black_box(
                     route_recognizer
@@ -93,12 +100,13 @@ fn compare_routers(c: &mut Criterion) {
         });
     });
 
-    let mut routefinder = routefinder::Router::new();
-    for route in register!(colon) {
-        routefinder.add(route, true).unwrap();
-    }
-    group.bench_function("routefinder", |b| {
-        b.iter(|| {
+    group.bench_function("routefinder", |bencher| {
+        let mut routefinder = routefinder::Router::new();
+        for route in register!(colon) {
+            routefinder.add(route, true).unwrap();
+        }
+
+        bencher.iter(|| {
             for route in black_box(call()) {
                 black_box(routefinder.best_match(route).unwrap());
             }
@@ -108,5 +116,5 @@ fn compare_routers(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, compare_routers);
+criterion_group!(benches, benchmark);
 criterion_main!(benches);
