1fbbe86fbabd1c118b726ecbdc68438382888376
Cathal Mullan <contact@cathal.dev>
2024-08-20 18:01:05 +0100

    Try and resolve performance regression to percent-decode function.

--- src/decode.rs
+++ src/decode.rs
@@ -12,8 +12,19 @@ pub(crate) fn percent_decode(input: &[u8]) -> Result<Cow<[u8]>, DecodeError> {
 
     while i < len {
         if input[i] == b'%' && i + 2 < len {
-            let character = [input[i], input[i + 1], input[i + 2]];
-            output.push(decode_hex(input, i, character)?);
+            let a = input[i + 1];
+            let b = input[i + 2];
+
+            if let Some(decoded) = decode_hex(a, b) {
+                output.push(decoded);
+            } else {
+                return Err(DecodeError::InvalidEncoding {
+                    input: String::from_utf8_lossy(input).to_string(),
+                    position: i,
+                    character: [b'%', a, b],
+                });
+            }
+
             i += 3;
         } else {
             output.push(input[i]);
@@ -24,25 +35,10 @@ pub(crate) fn percent_decode(input: &[u8]) -> Result<Cow<[u8]>, DecodeError> {
     Ok(Cow::Owned(output))
 }
 
+#[inline]
 #[allow(clippy::cast_possible_truncation)]
-fn decode_hex(input: &[u8], i: usize, character: [u8; 3]) -> Result<u8, DecodeError> {
-    let [_, a, b] = character;
-
-    let a = (a as char)
-        .to_digit(16)
-        .ok_or(DecodeError::InvalidEncoding {
-            input: String::from_utf8_lossy(input).to_string(),
-            position: i,
-            character,
-        })?;
-
-    let b = (b as char)
-        .to_digit(16)
-        .ok_or(DecodeError::InvalidEncoding {
-            input: String::from_utf8_lossy(input).to_string(),
-            position: i,
-            character,
-        })?;
-
-    Ok((a as u8) << 4 | (b as u8))
+fn decode_hex(a: u8, b: u8) -> Option<u8> {
+    let a = (a as char).to_digit(16)?;
+    let b = (b as char).to_digit(16)?;
+    Some((a as u8) << 4 | (b as u8))
 }
