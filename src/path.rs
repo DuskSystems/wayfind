4d0e69322951d614857b0ce7c84caa3384dccf8d
Cathal Mullan <contact@cathal.dev>
2024-08-20 18:25:13 +0100

    Remove raw from path

--- src/path.rs
+++ src/path.rs
@@ -2,14 +2,12 @@ use crate::{decode::percent_decode, errors::decode::DecodeError};
 use std::borrow::Cow;
 
 pub struct Path<'path> {
-    _raw: &'path [u8],
     decoded: Cow<'path, [u8]>,
 }
 
 impl<'path> Path<'path> {
     pub fn new(path: &'path str) -> Result<Self, DecodeError> {
         Ok(Self {
-            _raw: path.as_bytes(),
             decoded: percent_decode(path.as_bytes())?,
         })
     }
