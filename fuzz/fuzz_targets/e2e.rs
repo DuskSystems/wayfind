96e9b7c21c6425def40bc8c4ac7bbd12ababdc72
Cathal Mullan <contact@cathal.dev>
2024-08-29 16:14:53 +0100

    Update fuzzer to run end-to-end.

--- fuzz/fuzz_targets/e2e.rs
+++ fuzz/fuzz_targets/e2e.rs
@@ -1,10 +1,14 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
+use wayfind::{Path, Router};
 
 fuzz_target!(|data: &[u8]| {
-    let mut router = wayfind::Router::new();
+    let mut router = Router::new();
     if let Ok(route) = std::str::from_utf8(data) {
         let _ = router.insert(route, true);
+        if let Ok(path) = Path::new(route) {
+            let _ = router.search(&path);
+        }
     }
 });
