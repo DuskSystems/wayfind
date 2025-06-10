471e24807fab8a544bf4cd1c36d7a1b6634c08de
Cathal Mullan <contact@cathal.dev>
2025-06-10 16:22:58 +0100

    Remove criterion benchmarks

--- BENCHMARKING.md
+++ BENCHMARKING.md
@@ -25,9 +25,7 @@ As such, we provide 2 sets of results per benchmark:
 - one with the default behaviour of the router.
 - one with the parameters extracted to `SmallVec<[(&str, &str); 4]>`.
 
-These result tables combine both [`criterion`](https://github.com/bheisler/criterion.rs) and [`divan`](https://github.com/nvzqz/divan) output:
-- we take the 'point estimate' time from `criterion`.
-- we take the 'slowest' alloc/dealloc values from `divan`.
+We use [`divan`](https://github.com/nvzqz/divan) for our benchmarks, taking the 'median' results from its output to create the following tables.
 
 ### `matchit` inspired benches
 
@@ -35,27 +33,27 @@ In a router of 130 templates, benchmark matching 130 paths.
 
 #### Default
 
-| Library          | Time      | Alloc Count | Alloc Size | Dealloc Count | Dealloc Size |
-|:-----------------|----------:|------------:|-----------:|--------------:|-------------:|
-| matchit          | 5.4637 µs | 1           | 128 B      | 1             | 128 B        |
-| wayfind          | 7.6363 µs | 0           | n/a        | 0             | n/a          |
-| path-tree        | 8.9698 µs | 0           | n/a        | 0             | n/a          |
-| xitca-router     | 14.138 µs | 103         | 13.18 KB   | 103           | 13.18 KB     |
-| ntex-router      | 43.049 µs | 306         | 22.97 KB   | 306           | 22.97 KB     |
-| route-recognizer | 70.832 µs | 3596        | 195.6 KB   | 3596          | 195.6 KB     |
-| actix-router     | 546.53 µs | 6934        | 447.4 KB   | 6934          | 447.4 KB     |
+| Library          | Time     | Alloc Count | Alloc Size | Dealloc Count | Dealloc Size |
+|:-----------------|---------:|------------:|-----------:|--------------:|-------------:|
+| matchit          | 5.416 µs | 1           | 128 B      | 1             | 128 B        |
+| wayfind          | 7.499 µs | 0           | n/a        | 0             | n/a          |
+| path-tree        | 9.041 µs | 0           | n/a        | 0             | n/a          |
+| xitca-router     | 14.15 µs | 103         | 13.18 KB   | 103           | 13.18 KB     |
+| ntex-router      | 47.47 µs | 306         | 22.97 KB   | 306           | 22.97 KB     |
+| route-recognizer | 74.26 µs | 3596        | 195.6 KB   | 3596          | 195.6 KB     |
+| actix-router     | 592.9 µs | 6934        | 447.4 KB   | 6934          | 447.4 KB     |
 
 #### String Parameters
 
-| Library          | Time      | Alloc Count | Alloc Size | Dealloc Count | Dealloc Size |
-|:-----------------|----------:|------------:|-----------:|--------------:|-------------:|
-| wayfind          | 8.9127 µs | 0           | n/a        | 0             | n/a          |
-| matchit          | 10.003 µs | 1           | 128 B      | 1             | 128 B        |
-| path-tree        | 10.059 µs | 0           | n/a        | 0             | n/a          |
-| xitca-router     | 14.882 µs | 103         | 13.18 KB   | 103           | 13.18 KB     |
-| ntex-router      | 43.631 µs | 306         | 22.97 KB   | 306           | 22.97 KB     |
-| route-recognizer | 72.031 µs | 3596        | 195.6 KB   | 3596          | 195.6 KB     |
-| actix-router     | 544.45 µs | 6934        | 447.4 KB   | 6934          | 447.4 KB     |
+| Library          | Time     | Alloc Count | Alloc Size | Dealloc Count | Dealloc Size |
+|:-----------------|---------:|------------:|-----------:|--------------:|-------------:|
+| wayfind          | 7.957 µs | 0           | n/a        | 0             | n/a          |
+| matchit          | 9.499 µs | 1           | 128 B      | 1             | 128 B        |
+| path-tree        | 9.957 µs | 0           | n/a        | 0             | n/a          |
+| xitca-router     | 14.78 µs | 103         | 13.18 KB   | 103           | 13.18 KB     |
+| ntex-router      | 47.93 µs | 306         | 22.97 KB   | 306           | 22.97 KB     |
+| route-recognizer | 75.30 µs | 3596        | 195.6 KB   | 3596          | 195.6 KB     |
+| actix-router     | 564.9 µs | 6934        | 447.4 KB   | 6934          | 447.4 KB     |
 
 ### `path-tree` inspired benches
 
@@ -63,27 +61,27 @@ In a router of 320 templates, benchmark matching 80 paths.
 
 #### Default
 
-| Library          | Time      | Alloc Count | Alloc Size | Dealloc Count | Dealloc Size |
-|:-----------------|----------:|------------:|-----------:|--------------:|-------------:|
-| wayfind          | 3.9724 µs | 0           | n/a        | 0             | n/a          |
-| path-tree        | 4.9039 µs | 0           | n/a        | 0             | n/a          |
-| matchit          | 6.0203 µs | 81          | 10.36 KB   | 81            | 10.36 KB     |
-| xitca-router     | 9.2584 µs | 150         | 18.06 KB   | 150           | 18.06 KB     |
-| ntex-router      | 26.716 µs | 142         | 12.09 KB   | 142           | 12.09 KB     |
-| route-recognizer | 65.154 µs | 2813        | 184.2 KB   | 2813          | 197.4 KB     |
-| actix-router     | 185.50 µs | 2142        | 121.4 KB   | 2142          | 121.4 KB     |
+| Library          | Time     | Alloc Count | Alloc Size | Dealloc Count | Dealloc Size |
+|:-----------------|---------:|------------:|-----------:|--------------:|-------------:|
+| wayfind          | 3.874 µs | 0           | n/a        | 0             | n/a          |
+| path-tree        | 4.874 µs | 0           | n/a        | 0             | n/a          |
+| matchit          | 6.116 µs | 81          | 10.36 KB   | 81            | 10.36 KB     |
+| xitca-router     | 9.359 µs | 150         | 18.06 KB   | 150           | 18.06 KB     |
+| ntex-router      | 28.23 µs | 142         | 12.09 KB   | 142           | 12.09 KB     |
+| route-recognizer | 67.50 µs | 2813        | 184.2 KB   | 2813          | 197.4 KB     |
+| actix-router     | 228.0 µs | 2142        | 121.4 KB   | 2142          | 121.4 KB     |
 
 #### String Parameters
 
-| Library          | Time      | Alloc Count | Alloc Size | Dealloc Count | Dealloc Size |
-|:-----------------|----------:|------------:|-----------:|--------------:|-------------:|
-| wayfind          | 4.5999 µs | 0           | n/a        | 0             | n/a          |
-| path-tree        | 5.4243 µs | 0           | n/a        | 0             | n/a          |
-| matchit          | 8.1373 µs | 81          | 10.36 KB   | 81            | 10.36 KB     |
-| xitca-router     | 9.5970 µs | 150         | 18.06 KB   | 150           | 18.06 KB     |
-| ntex-router      | 27.036 µs | 142         | 12.09 KB   | 142           | 12.09 KB     |
-| route-recognizer | 65.873 µs | 2813        | 184.2 KB   | 2813          | 197.4 KB     |
-| actix-router     | 185.70 µs | 2142        | 121.4 KB   | 2142          | 121.4 KB     |
+| Library          | Time     | Alloc Count | Alloc Size | Dealloc Count | Dealloc Size |
+|:-----------------|---------:|------------:|-----------:|--------------:|-------------:|
+| wayfind          | 4.207 µs | 0           | n/a        | 0             | n/a          |
+| path-tree        | 5.416 µs | 0           | n/a        | 0             | n/a          |
+| matchit          | 7.991 µs | 81          | 10.36 KB   | 81            | 10.36 KB     |
+| xitca-router     | 9.775 µs | 150         | 18.06 KB   | 150           | 18.06 KB     |
+| ntex-router      | 28.56 µs | 142         | 12.09 KB   | 142           | 12.09 KB     |
+| route-recognizer | 68.38 µs | 2813        | 184.2 KB   | 2813          | 197.4 KB     |
+| actix-router     | 190.8 µs | 2142        | 121.4 KB   | 2142          | 121.4 KB     |
 
 ## `wayfind` benches
 
--- benches/path_tree.rs
+++ benches/path_tree.rs
@@ -1,11 +1,15 @@
-//! Benches sourced from `matchit` (MIT AND BSD-3-Clause)
-//! <https://github.com/ibraheemdev/matchit/blob/v0.8.6/benches/bench.rs>
+#![allow(clippy::incompatible_msrv)]
 
-use criterion::black_box;
+//! Benches sourced from `path-tree` (MIT OR Apache-2.0)
+//! <https://github.com/viz-rs/path-tree/blob/v0.8.1/benches/bench.rs>
+
+use std::hint::black_box;
+
 use divan::AllocProfiler;
+use path_tree_routes::paths;
 use smallvec::SmallVec;
 
-pub mod matchit_routes;
+pub mod path_tree_routes;
 
 #[global_allocator]
 static ALLOC: AllocProfiler = AllocProfiler::system();
@@ -17,12 +21,12 @@ fn main() {
 #[divan::bench(name = "wayfind")]
 fn wayfind(bencher: divan::Bencher<'_, '_>) {
     let mut router = wayfind::Router::new();
-    for route in routes!(brackets) {
-        router.insert(route, true).unwrap();
+    for (index, route) in routes!(brackets).iter().enumerate() {
+        router.insert(route, index).unwrap();
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let _output = black_box(router.search(black_box(path)).unwrap());
         }
     });
@@ -31,12 +35,12 @@ fn wayfind(bencher: divan::Bencher<'_, '_>) {
 #[divan::bench(name = "wayfind (parameters)")]
 fn wayfind_params(bencher: divan::Bencher<'_, '_>) {
     let mut router = wayfind::Router::new();
-    for route in routes!(brackets) {
-        router.insert(route, true).unwrap();
+    for (index, route) in routes!(brackets).iter().enumerate() {
+        router.insert(route, index).unwrap();
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let output = black_box(router.search(black_box(path)).unwrap());
             let _parameters: SmallVec<[(&str, &str); 4]> =
                 black_box(output.parameters.iter().map(|p| (p.0, p.1)).collect());
@@ -53,7 +57,7 @@ fn actix_router(bencher: divan::Bencher<'_, '_>) {
     let router = router.finish();
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let mut path = actix_router::Path::new(path);
             let _output = black_box(router.recognize(black_box(&mut path)).unwrap());
         }
@@ -69,7 +73,7 @@ fn actix_router_params(bencher: divan::Bencher<'_, '_>) {
     let router = router.finish();
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let mut path = actix_router::Path::new(path);
             let _output = black_box(router.recognize(black_box(&mut path)).unwrap());
             let _parameters: SmallVec<[(&str, &str); 4]> =
@@ -81,12 +85,12 @@ fn actix_router_params(bencher: divan::Bencher<'_, '_>) {
 #[divan::bench(name = "matchit")]
 fn matchit(bencher: divan::Bencher<'_, '_>) {
     let mut router = matchit::Router::new();
-    for route in routes!(brackets) {
-        router.insert(route, true).unwrap();
+    for (index, route) in routes!(brackets).iter().enumerate() {
+        router.insert(*route, index).unwrap();
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let _output = black_box(router.at(black_box(path)).unwrap());
         }
     });
@@ -95,12 +99,12 @@ fn matchit(bencher: divan::Bencher<'_, '_>) {
 #[divan::bench(name = "matchit (parameters)")]
 fn matchit_params(bencher: divan::Bencher<'_, '_>) {
     let mut router = matchit::Router::new();
-    for route in routes!(brackets) {
-        router.insert(route, true).unwrap();
+    for (index, route) in routes!(brackets).iter().enumerate() {
+        router.insert(*route, index).unwrap();
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let output = black_box(router.at(black_box(path)).unwrap());
             let _parameters: SmallVec<[(&str, &str); 4]> =
                 black_box(output.params.iter().map(|p| (p.0, p.1)).collect());
@@ -117,7 +121,7 @@ fn ntex_router(bencher: divan::Bencher<'_, '_>) {
     let router = router.finish();
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let mut path = ntex_router::Path::new(path);
             let _output = router.recognize(&mut path).unwrap();
         }
@@ -133,9 +137,9 @@ fn ntex_router_params(bencher: divan::Bencher<'_, '_>) {
     let router = router.finish();
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let mut path = ntex_router::Path::new(path);
-            let _output = router.recognize(&mut path).unwrap();
+            router.recognize(&mut path).unwrap();
             let _parameters: SmallVec<[(&str, &str); 4]> =
                 black_box(path.iter().map(|p| (p.0, p.1)).collect());
         }
@@ -145,12 +149,12 @@ fn ntex_router_params(bencher: divan::Bencher<'_, '_>) {
 #[divan::bench(name = "path-tree")]
 fn path_tree(bencher: divan::Bencher<'_, '_>) {
     let mut router = path_tree::PathTree::new();
-    for route in routes!(colon) {
-        let _ = router.insert(route, true);
+    for (index, route) in routes!(colon).iter().enumerate() {
+        let _ = router.insert(route, index);
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let _output = black_box(router.find(path).unwrap());
         }
     });
@@ -159,12 +163,12 @@ fn path_tree(bencher: divan::Bencher<'_, '_>) {
 #[divan::bench(name = "path-tree (parameters)")]
 fn path_tree_params(bencher: divan::Bencher<'_, '_>) {
     let mut router = path_tree::PathTree::new();
-    for route in routes!(colon) {
-        let _ = router.insert(route, true);
+    for (index, route) in routes!(colon).iter().enumerate() {
+        let _ = router.insert(route, index);
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let output = router.find(path).unwrap();
             let _parameters: SmallVec<[(&str, &str); 4]> =
                 black_box(output.1.params_iter().map(|p| (p.0, p.1)).collect());
@@ -174,13 +178,13 @@ fn path_tree_params(bencher: divan::Bencher<'_, '_>) {
 
 #[divan::bench(name = "route-recognizer")]
 fn route_recognizer(bencher: divan::Bencher<'_, '_>) {
-    let mut router = route_recognizer::Router::new();
-    for route in routes!(colon) {
-        router.add(route, true);
+    let mut router = route_recognizer::Router::<usize>::new();
+    for (index, route) in routes!(colon).iter().enumerate() {
+        router.add(route, index);
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let _output = black_box(router.recognize(path).unwrap());
         }
     });
@@ -188,13 +192,13 @@ fn route_recognizer(bencher: divan::Bencher<'_, '_>) {
 
 #[divan::bench(name = "route-recognizer (parameters)")]
 fn route_recognizer_params(bencher: divan::Bencher<'_, '_>) {
-    let mut router = route_recognizer::Router::new();
-    for route in routes!(colon) {
-        router.add(route, true);
+    let mut router = route_recognizer::Router::<usize>::new();
+    for (index, route) in routes!(colon).iter().enumerate() {
+        router.add(route, index);
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let output = router.recognize(path).unwrap();
             let _parameters: SmallVec<[(&str, &str); 4]> =
                 black_box(output.params().iter().map(|p| (p.0, p.1)).collect());
@@ -205,12 +209,12 @@ fn route_recognizer_params(bencher: divan::Bencher<'_, '_>) {
 #[divan::bench(name = "xitca-router")]
 fn xitca_router(bencher: divan::Bencher<'_, '_>) {
     let mut router = xitca_router::Router::new();
-    for route in routes!(colon) {
-        router.insert(route, true).unwrap();
+    for (index, route) in routes!(colon).iter().enumerate() {
+        router.insert(*route, index).unwrap();
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let _output = black_box(router.at(path).unwrap());
         }
     });
@@ -219,12 +223,12 @@ fn xitca_router(bencher: divan::Bencher<'_, '_>) {
 #[divan::bench(name = "xitca-router (parameters)")]
 fn xitca_router_params(bencher: divan::Bencher<'_, '_>) {
     let mut router = xitca_router::Router::new();
-    for route in routes!(colon) {
-        router.insert(route, true).unwrap();
+    for (index, route) in routes!(colon).iter().enumerate() {
+        router.insert(*route, index).unwrap();
     }
 
     bencher.bench(|| {
-        for path in black_box(routes!(literal)) {
+        for path in black_box(paths()) {
             let output = router.at(path).unwrap();
             let _parameters: SmallVec<[(&str, &str); 4]> =
                 black_box(output.params.iter().map(|p| (p.0, p.1)).collect());
