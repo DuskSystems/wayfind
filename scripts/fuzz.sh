ecac10063b93e89f0eccb26ddb0abb5c29f1067a
Cathal Mullan <contact@cathal.dev>
2026-01-18 13:17:09 +0000

    Set fuzzer timeout per input

--- scripts/fuzz.sh
+++ scripts/fuzz.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env -S nix develop .#ci-nightly --command bash
 set -euxo pipefail
 
-TIME="${1:-15}"
+TIME="${1:-60}"
 for TARGET in $(cargo fuzz list); do
-  cargo fuzz run "${TARGET}" --sanitizer none -- -max_total_time="${TIME}" -jobs="$(nproc)"
+  cargo fuzz run "${TARGET}" \
+    --sanitizer none \
+    -- \
+    -timeout=1 \
+    -max_total_time="${TIME}" \
+    -jobs="$(nproc)"
 done
